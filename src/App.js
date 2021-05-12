b82d042ac087bb229cb66edc34560c4e0001f004
Anthonalex <Anthonalex@users.noreply.github.com>
2021-05-12 23:35:54 +0400

    fix: correct font-size of buttons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Button from "./components/Button/Button";
 function App() {
   return (
     <div className="flex-box">
+      <Button buttonStyle={"btn--primary--normal"}>Hello world!</Button>
+      <Button buttonStyle={"btn--primary--destructive"}>Hello world!</Button>
+      <Button buttonStyle={"btn--secondary--normal"}>Hello world!</Button>
+      <Button buttonStyle={"btn--secondary--destructive"}>Hello world!</Button>
+      <Button buttonStyle={"btn--subtle--normal"}>Hello world!</Button>
       <Button buttonStyle={"btn--subtle--destructive"}>Hello world!</Button>
     </div>
   );
