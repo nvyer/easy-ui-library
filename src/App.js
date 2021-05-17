d31b7aa4bc0de2468279a7ff834638212bc8c30f
Anthonalex <Anthonalex@users.noreply.github.com>
2021-05-17 12:46:58 +0400

    fix: correct and add loader style and css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import "./App.css";
-import Button from "./components/Button/Button";
+
 
 function App() {
-  return (
-    <div className="flex-box">
-      <Button buttonStyle={"btn--primary--normal"}>Hello world!</Button>
-      <Button buttonStyle={"btn--primary--destructive"}>Hello world!</Button>
-      <Button buttonStyle={"btn--secondary--normal"}>Hello world!</Button>
-      <Button buttonStyle={"btn--secondary--destructive"}>Hello world!</Button>
-      <Button buttonStyle={"btn--subtle--normal"}>Hello world!</Button>
-      <Button buttonStyle={"btn--subtle--destructive"}>Hello world!</Button>
-      <Button buttonStyle={"btn--primary--normal"} isLoading={true}>hello world</Button>
-      <Button buttonStyle={"btn--primary--destructive"} isLoading={true}>hello world</Button>
-      <Button buttonStyle={"btn--secondary--normal"} isLoading={true}>hello world</Button>
-      <Button buttonStyle={"btn--secondary--destructive"} isLoading={true}>hello world</Button>
-      <Button buttonStyle={"btn--subtle--normal"} isLoading={true}>hello world</Button>
-      <Button buttonStyle={"btn--subtle--destructive"} isLoading={true}>hello world</Button>
-
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -13,6 +13,30 @@ const STYLES = [
 
 const Button = ({ children, onClick, buttonStyle, disabled, isLoading }) => {
   const checkBtnStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+  let checkLoaderStyle;
+  switch (buttonStyle) {
+    case "btn--primary--normal":
+      checkLoaderStyle = "primary--normall";
+      break;
+    case "btn--primary--destructive":
+      checkLoaderStyle = "primary--destructive";
+      break;
+    case "btn--secondary--normal":
+      checkLoaderStyle = "secondary--normal";
+      break;
+    case "btn--secondary--destructive":
+      checkLoaderStyle = "secondary--destructive";
+      break;
+    case "btn--subtle--normal":
+      checkLoaderStyle = "subtle--normal";
+      break;
+    case "btn--subtle--destructive":
+      checkLoaderStyle = "subtle--destructive";
+      break;
+    default:
+      checkLoaderStyle = "primary--normall";
+      break;
+  }
 
   return (
     <button
@@ -21,7 +45,7 @@ const Button = ({ children, onClick, buttonStyle, disabled, isLoading }) => {
       disabled={disabled}
     >
       {!isLoading && children}
-      {isLoading && <div className="loader"></div> }
+      {isLoading && <div className={`loader ${checkLoaderStyle}`}></div>}
     </button>
   );
 };
