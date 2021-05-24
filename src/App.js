9b9e87ac7a7c2635cefcafe11419c9b1061961da
Anthonalex <Anthonalex@users.noreply.github.com>
2021-05-24 18:10:53 +0400

    fix: clear app.js and app.css file, for merge

--- src/App.js
+++ src/App.js
@@ -3,45 +3,8 @@ import TextFieldInput from "./components/Input/Input";
 
 function App() {
   return (
-    <div className="App">
-      <TextFieldInput label="Standard" />
-      <TextFieldInput label="Filled" variant="filled" />
-      <TextFieldInput label="outlined" variant="outlined" />
-      <TextFieldInput
-        label="required field"
-        variant="outlined"
-        required={true}
-      />
-      <TextFieldInput label="disabled" variant="outlined" disabled />
-      <TextFieldInput label="password" variant="outlined" type="password" />
-      <TextFieldInput label="searchfield" variant="outlined" type="search" />
-      <TextFieldInput label="Number" variant="outlined" type="number" />
-      <TextFieldInput
-        label="READ only"
-        variant="outlined"
-        type="text"
-        readonly="readonly"
-        defaultValue="Read only"
-      />
-      <TextFieldInput
-        label="READ only"
-        variant="outlined"
-        type="text"
-        readOnly="readonly"
-        defaultValue="Read only with helperText"
-        helperText="some helper text some helper text"
-      />
-      <TextFieldInput
-        label="ERROR"
-        variant="outlined"
-        error={true}
-        helperText="error massage"
-      />
-      <TextFieldInput
-        label="Standard"
-        error={true}
-        helperText="error massage"
-      />
+    <div>
+      
     </div>
   );
 }
