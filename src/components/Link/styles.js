feea9093be2820859b3a8844af8a9ebd8314ac5f
nyer <nyeritsian@gmail.com>
2021-06-20 14:03:55 +0400

    fix(components):bring components back

---
+++ src/components/Link/styles.js
@@ -0,0 +1,24 @@
+import { createUseStyles } from "react-jss";
+
+export const useLinkStyles = createUseStyles({
+    link: color => ({
+        textDecoration: "none",
+        fontSize: "20px",
+        padding: "10px",
+        color: color === "primary" ? "#1DD4CE" : "#2C3E50",
+        "&:link": {
+            textDecoration: "none",
+            fontSize: "20px",
+        },
+        "&:visited": {
+            color: color === "primary" ? "#1DD4CE" : "#2C3E50"
+        },
+        "&:hover": {
+            textDecoration: "underline",
+            paddingBottom: "10px",
+            textUnderlineOffset: "2.5px",
+            textDecorationThickness: ".125em",
+            textDecorationColor: color === "primary" ? "#1DD4CE" : "#2C3E50",
+        }
+    })
+})
