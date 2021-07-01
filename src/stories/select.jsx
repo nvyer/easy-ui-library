a88da14e70d5c197d826e9b3ec284d35b7233913
Harut Qanaqaryan <hqanaqaryan@gmail.com>
2021-07-01 18:30:23 +0300

    refactor: normalized story functions

---
+++ src/stories/select.jsx
@@ -0,0 +1,80 @@
+import Select from "../components/Select/select";
+import { Option, GroupOptions } from "../components/Select/select";
+
+const selectStory = () => {
+    const selectOneOption = `<Select placeholder="Select">
+    <Option>Google</Option>
+    <Option>Facebook</Option>
+  </Select>`;
+
+  const selectMultypleOptionList = `<Select placeholder="Select" multypleType="list">
+  <Option id={1}>Google</Option>
+  <Option id={2}>Facebook</Option>
+</Select>`;
+
+  const selectMultypleOptionBlock = `<Select placeholder="Select" multypleType="block">
+<Option id={1}>Google</Option>
+<Option id={2}>Facebook</Option>
+</Select>`;
+
+  const groupOptions = `<Select placeholder="Select">
+<GroupOptions title="Select">
+  <Option>Google</Option>
+  <Option>Facebook</Option>
+</GroupOptions>
+<GroupOptions title="Select-2">
+  <Option>Google</Option>
+  <Option>Facebook</Option>
+</GroupOptions>
+</Select>`;
+  return (
+    <>
+      <h4>
+        Props: placeholder, value, onChange, id, multypleType = list,block{" "}
+      </h4>
+      <div className="component-container">
+        <p className="component-title">Select one Option</p>
+        <Select placeholder="Select">
+          <Option id={1}>Google</Option>
+          <Option id={2}>Facebook</Option>
+        </Select>
+        <h3 className="component-code">{selectOneOption}</h3>
+      </div>
+
+      <div className="component-container">
+        <p className="component-title">Select multyple Options(List)</p>
+        <Select placeholder="Select" multypleType="list">
+          <Option id={1}>Google</Option>
+          <Option id={2}>Facebook</Option>
+        </Select>
+        <h3 className="component-code">{selectMultypleOptionList}</h3>
+      </div>
+
+      <div className="component-container">
+        <p className="component-title">Select multyple Options(block)</p>
+        <Select placeholder="Select" multypleType="block">
+          <Option id={1}>Google</Option>
+          <Option id={2}>Facebook</Option>
+        </Select>
+        <h3 className="component-code">{selectMultypleOptionBlock}</h3>
+      </div>
+
+      <div className="component-container">
+        <p className="component-title">Group Options</p>
+        <Select placeholder="Select">
+          <GroupOptions title="Select">
+            <Option>Google</Option>
+            <Option>Facebook</Option>
+          </GroupOptions>
+          <GroupOptions title="Select-2">
+            <Option>Google</Option>
+            <Option>Facebook</Option>
+          </GroupOptions>
+        </Select>
+        <h3 className="component-code">{groupOptions}</h3>
+      </div>
+    </>
+  );
+}
+
+export default selectStory;
---
+++ src/stories/tooltip.jsx
@@ -0,0 +1,57 @@
+import Tooltip from "../components/Tooltip/tooltip";
+import Button from "../components/Button/Button";
+
+const tooltipStory = () => {
+    const primaryTooltip = `<Tooltip tooltipStyle="tooltip--primary" content="Hello" position="top">
+   <Button>Easy-UI</Button>
+   </Tooltip>`;
+
+  const destructiveTooltip = `<Tooltip tooltipStyle="tooltip--destructive" content="Hello" position="bottom">
+   <Button>Easy-UI</Button>
+   </Tooltip>`;
+
+  const neutralTooltip = `<Tooltip tooltipStyle="tooltip--neutral" content="Hello" position="top--right">
+   <Button>Easy-UI</Button>
+   </Tooltip>`;
+  return (
+    <>
+      <h4>
+        Props: tooltipStyle, position(top, bottom, right, left, top--right,
+        top--left, bottom--left, bottom--right), content
+      </h4>
+      <div className="component-container">
+        <p className="component-title">Primary</p>
+        <Tooltip tooltipStyle="tooltip--primary" content="Hello" position="top">
+          <Button>Easy-UI</Button>
+        </Tooltip>
+        <h3 className="component-code">{primaryTooltip}</h3>
+      </div>
+
+      <div className="component-container">
+        <p className="component-title">Destructive</p>
+        <Tooltip
+          tooltipStyle="tooltip--destructive"
+          content="Hello"
+          position="bottom"
+        >
+          <Button>Easy-UI</Button>
+        </Tooltip>
+        <h3 className="component-code">{destructiveTooltip}</h3>
+      </div>
+
+      <div className="component-container">
+        <p className="component-title">Neutral</p>
+        <Tooltip
+          tooltipStyle="tooltip--neutral"
+          content="Hello"
+          position="top--right"
+        >
+          <Button>Easy-UI</Button>
+        </Tooltip>
+        <h3 className="component-code">{neutralTooltip}</h3>
+      </div>
+    </>
+  );
+}
+
+export default tooltipStory;
