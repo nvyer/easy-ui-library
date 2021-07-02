f538f3da8f20c08eda64075b61bbd6ab4d3333ba
Harut Qanaqaryan <hqanaqaryan@gmail.com>
2021-07-02 14:47:48 +0300

    setup rollup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-reportWebVitals();
+export * from "./components/BreadCrumbs/breadcrumbs";
+export * from "./components/Button/Button";
+export * from "./components/CheckBox/checkBox";
+export * from "./components/DropDown/dropdown";
+export * from "./components/Input/Input";
+export * from "./components/Link/link";
+export * from "./components/Loader/loader";
+export * from "./components/Select/native-select";
+export * from "./components/Select/select";
+export * from "./components/Slider/slider";
+export * from "./components/Stepper/stepper";
+export * from "./components/Stepper/step";
+export * from "./components/Stepper/stepLabel";
+export * from "./components/Stepper/stepConnector";
+export * from "./components/Table/tableContainer";
+export * from "./components/Table/table";
+export * from "./components/Table/tableHead";
+export * from "./components/Table/tableBody";
+export * from "./components/Table/tableRow";
+export * from "./components/Table/tableCell";
+export * from "./components/Table/tableFooter";
+export * from "./components/Table/tablePagination";
+export * from "./components/Tag/tag";
+export * from "./components/TimePicker/timePicker";
+export * from "./components/Tooltip/tooltip";
+export * from "./components/badge/Badge"
